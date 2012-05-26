93ca9fbe2766592126c08ac29ab7575d587b6258
David <david@MacBook-Pro-de-David.local>
2012-05-26 14:47:10 -0600

    rutas new,show y sus erb

--- tasks/config/routes.rb
+++ tasks/config/routes.rb
@@ -1,3 +1,17 @@
 Tasks::Application.routes.draw do
+ # get "tasks/index"
+
+ # get "tasks/show"
+
+ # get "tasks/new"
+
+ # get "tasks/create"
+
+ # get "tasks/edit"
+
+ # get "tasks/update"
+
+ # get "tasks/destroy"
+
   resources :tasks
 end
